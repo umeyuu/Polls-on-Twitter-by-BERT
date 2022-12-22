18465fa073c0217066b934244fc4e4c9042b6b75
umeyuu <umeyu7777777@gmail.com>
2022-12-22 19:59:24 +0900

    更新

--- src/setting.py
+++ src/setting.py
@@ -1,5 +1,5 @@
 
-CK = '****' # Consumer Key
-CS = '****' # Consumer Secret
-AT = '****' # Access Token
-AS = '****' # Accesss Token Secert
+CK = 'nvcAlVYRMyXInqTEw8VyOJMDB' # Consumer Key
+CS = 'rqrdv8nZqmbfAjjDNUoYNdLl0jpmU4LQxNneIx28F5mcoAo7hL' # Consumer Secret
+AT = '976641317718540288-5uQ5sPMGXMKNOksNcCmg33HjK4ToyyA' # Access Token
+AS = 'y4PXiRugBYlAy3Biv83TSLiPwN5hdJDJgNBkT9ZxIRqxi' # Accesss Token Secert
